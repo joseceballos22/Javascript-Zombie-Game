57b6439f6f988fe99637d9b673c35b2acf1710c6
Jose C <josec5064@student.vvc.edu>
2021-02-01 22:45:21 -0800

    Initial Code

---
+++ main.js
@@ -0,0 +1,63 @@
+
+/**
+ * Represents the Zombie
+ */
+class Zombie {
+    constructor() {
+
+    }
+
+    /**
+     * Updates all the components a Zombie has 
+     */
+    updateComponents() {
+
+    }
+}
+
+/**
+ * Represents the Humans
+ */
+class Human {
+    constructor() {
+
+    }
+
+    /**
+     * Updates all the components a human has 
+     */
+    updateComponents() {
+
+    }
+}
+
+/**
+ * The Actual Game 
+ */
+class Game {
+    constructor() {
+
+    }
+    /**
+     * Starts the Game 
+     */
+    start() {
+
+    }
+
+    /**
+     * Updates All the Components of the Game 
+     */
+    update() {
+
+    }
+}
+
+
+/**
+ * Main Function Which Will Start the Game 
+ */
+function main() {
+
+}
+main(); 
